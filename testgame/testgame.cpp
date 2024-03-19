6c9eeed80f1ab0bcd2e7ea0278164187595872a6
cursedenok <rnasibov52@gmail.com>
2024-03-19 23:36:06 +0200

    progress is going

--- testgame/testgame.cpp
+++ testgame/testgame.cpp
@@ -1,44 +1,73 @@
 ﻿#include <iostream>
 #include<conio.h>
-#include "inventory.h"
+#include<thread>
 #include "things.h"
 
-Player player;
+using namespace std;
+
+
+Player* player = new Player();
 GameField gameField;
-Rope rope;
-Pickaxe pickaxe;
-Level lvl;
+Rope* rope = new Rope();
+Axe* axe = new Axe();
+Level* lvl = new Level();
 void detectMoving() {
+    
     while (true) {
+        int x = player->getX();
+        int y = player->getY();
+        
         if (_kbhit()) {
-            char ch = _getch();
+            char ch = tolower(_getch());
             switch (ch) {
-            case 72:
-                player.move(0, -1);
+            case 'a':
+                gameField.movePlayer(player, -1, 0);
                 break;
-            case 80:
-                player.move(0, 1);
+            case 'd':
+                gameField.movePlayer(player, 1, 0);
                 break;
-            case 75:
-                player.move(-1, 0);
+            case 'w':
+                gameField.movePlayer(player, 0, -1);
                 break;
-            case 77:
-                player.move(1, 0);
+            case 's':
+                gameField.movePlayer(player, 0, 1);
                 break;
             default:
                 break;
             }
+            gameField.drawField(player, axe, lvl); // Обновляем поле после каждого действия игрока
+            cout << x << "   " << y << endl;
+            cout << endl << axe->getX() << "   " << axe->getY() << endl;
+            cout << endl << lvl->getChestX() << "   " << lvl->getChestY() << endl;
+            cout << endl << lvl->getRopePlX() << "   " << lvl->getRopePlY() << endl;
+            player->displayInv();
         }
+    }
+    
+}
+void checkLocation() {
+    if (player->getX() == axe->getX() && player->getY() == axe->getY())
+    {
+        player->addItem(axe);
+        axe->setX(1000);
+        axe->setY(1000);
+    }
+    if (player->getX() == lvl->getChestX() && lvl->getChestY() == player->getY() && player->hasItem(axe))
+    {
+        lvl->destroy(player, rope);
+    }
+    if (player->getX() == lvl->getRopePlX() && player->getY() == lvl->getChestY() && player->hasItem(rope))
+    {
+        lvl->resetLvL(player);
     }
 }
 
-int main()
-{
+int main() {
+    thread Moving(detectMoving);
     while (true)
     {
-        gameField.drawField();
-        detectMoving();
-
+        checkLocation();
     }
-    std::cout << "Hello World!\n";
+
+    Moving.join();
 }
--- testgame/things.h
+++ testgame/things.h
@@ -1,37 +1,191 @@
 #include <iostream>
-#include "inventory.h"
+#include<algorithm>
+#include<vector>
+#include<string>
+#include <conio.h>
+#include<Windows.h>
+
 using namespace std;
-class Player;
-class Gamefield;
-class Level {
-private:
-	int wallX; // walls will be everywhere, cause it will be a cave, but wall with rope in it will be only one
-	int wallY;
-	int ropePlaceX; // coordinates where rope will have to be placed, to escape the cave
-	int ropePlaceY;
-	bool wallIsDestroyed = false;
+
+class Item {
+protected:
+	string name;
+	string description;
 public:
-	void destroy(invetory* inv, int playerX, int playerY) {
-		if (playerX == wallX && playerY == wallY) {
-			wallIsDestroyed = true;
-			Rope* rope = new Rope();
-			inv->addItem(rope);
-		}
-		else
-			cout << "You are too far from the wall" << endl;
+	Item(const string& itemName, const string& itemDescription) : name(itemName), description(itemDescription) {}
+
+	virtual void use() {
+		cout << "This item cannot be used" << endl;
 	}
-	int getWallX(){
-		return wallX;
+
+	const string& getName() const {
+		return name;
 	}
-	int getWallY(){
-		return wallY;
+	const string& getDescription() const {
+		return description;
 	}
 };
+
+class Rope : public Item {
+private:
+
+public:
+	Rope() : Item("Rope", "A good rope to climb somewhere down") {}
+
+	void use() override {
+		cout << "You used the rope" << endl;
+	}
+	
+};
+
+class Axe : public Item {
+private:
+	int axeX = rand() % 48 + 1;
+	int axeY = rand() % 48 + 1;
+public:
+	Axe() : Item("Axe", "Seems like it can destroy some chests") {}
+
+	void use() override {
+		cout << "You used the axe" << endl;;
+	}
+	int getX() {
+		return axeX;
+	}
+	int getY() {
+		return axeY;
+	}
+	void setX(int dx) {
+		axeX = dx;
+	}
+	void setY(int dy) {
+		axeY = dy;
+	}
+};
+
+class inventory
+{
+private:
+	vector<Item*> items;
+public:
+	void resetInv() {
+		items.clear();
+	}
+	void addItem(Item* item) {
+		items.push_back(item);
+		cout << "The" << item->getName() << "was added to your invenory!";
+	}
+	void displayInventory() const {
+		for (const auto& item : items)
+		{
+			cout << "Your inventory:" << endl;
+			cout << item->getName() << ": " << item->getDescription();
+		}
+	}
+	bool containsItem(Item* itemToFind)  const {
+		for (auto it = 0; it < items.size(); it++)
+		{
+			if (items[it] == itemToFind)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+};
+
+class Player {
+private:
+	int playerX = 5;
+	int playerY = 5;
+	inventory inventory;
+public:
+	void move(int dx, int dy) {
+		playerX += dx;
+		playerY += dy;
+	}
+	void displayInv() {
+		inventory.displayInventory();
+	}
+	void resetInv() {
+		inventory.resetInv();
+	}
+	void addItem(Item* item) {
+		inventory.addItem(item);
+	}
+	
+	bool hasItem(Item* item) const {
+		return inventory.containsItem(item);
+	}
+	int getX() {
+		return playerX;
+	}
+	int getY() {
+		return playerY;
+	}
+	void setY(int playerY) {
+		this->playerY = playerY;
+	}
+	void setX(int playerX) {
+		this->playerX = playerX;
+	}
+};
+
+class Level {
+private:
+	int chestX = rand() % 48 + 1;
+	int chestY = rand() % 48 + 1;
+	int ropePlaceX = rand() % 48 + 1; 
+	int ropePlaceY = rand() % 48 + 1;
+	
+public:
+	void destroy(Player* player, Rope* rope) {
+		if (player->getX() == chestX && player->getY() == chestY) {
+			player->addItem(rope);
+			setChestX(1000);
+			setChestY(1000);
+		}
+	}
+	void resetLvL(Player* player){
+		player->resetInv();
+		chestX = rand() % 48 + 1;
+		chestY = rand() % 48 + 1;
+		ropePlaceX = rand() % 48 + 1;
+		ropePlaceY = rand() % 48 + 1;
+		player->setX(rand() % 48 + 1);
+		player->setY(rand() % 48 + 1);
+	}
+	int getRopePlX() {
+		return ropePlaceX;
+	}
+	int getRopePlY() {
+		return ropePlaceY;
+	}
+	int getChestX(){
+		return chestX;
+	}
+	int getChestY(){
+		return chestY;
+	}
+	void setRopePlX(int x) {
+		ropePlaceX = x;
+	}
+	void setRopePlY(int y) {
+		ropePlaceY = y;
+	}
+	void setChestX(int x) {
+		chestX = x;
+	}
+	void setChestY(int y) {
+		chestY = y;
+	}
+};
+
 class GameField {
 private:
-	const int width = 10;
-	const int height = 10;
-	char field[10][10];
+	const int width = 50;
+	const int height = 50;
+	const int viewRadius = 1;
+	char field[50][50];
 
 public:
 	GameField() {
@@ -41,16 +195,58 @@ public:
 			}
 		}
 	}
-	void drawField() const {
-		for (int i = 0; i < height; i++) {
-			for (int j = 0; j < width; j++) {
-				cout << field[i][j];
+	void drawField(Player* player, Axe* pickaxe, Level* lvl) {
+		system("cls");
+		int playerX = player->getX();
+		int playerY = player->getY();
+		
+		for (int y = 0; y <= height; y++) {
+			for (int x = 0; x <= width; x++) {
+				if (abs(x - playerX) <= viewRadius && abs(y - playerY) <= viewRadius) {
+					if (x == playerX && y == playerY) {
+						cout << 'P'; // Отображаем игрока
+					}
+					else if (x == pickaxe->getX() && y == pickaxe->getY()) {
+						cout << 'A';
+					}
+					else if (x == lvl->getChestX() && y == lvl->getChestY()) {
+						cout << 'C';
+					}
+					else if (x == lvl->getRopePlX() && y == lvl->getRopePlY())
+					{
+						cout << 'O';
+					}
+					else
+					{
+						if (field[x][y] == '#')
+						{
+							field[x][y] = ' ';
+							cout << field[x][y]; // Отображаем символы поля
+						}
+						else {
+							cout << field[x][y];
+						}
+						
+					}
+				}
+				else {
+					if (x < 50 && x > 0 && y > 0 && y < 50)
+					{
+						cout << '#'; // За пределами области видимости отображаем стену
+					}
+					else
+					{
+						cout << 'W';
+					}
+				}
 			}
 			cout << endl;
 		}
+		
 	}
 	void placePlayer(Player* player, int x, int y) {
-		field[y][x] = 'P';
+		player->setX(x);
+		player->setY(y);
 	}
 	void movePlayer(Player* player, int dx, int dy) {
 		int oldX = player->getX();
@@ -59,12 +255,9 @@ public:
 		int newY = oldY + dy;
 
 
-		if (newX >= 0 && newX < width && newY >= 0 && newY < height && field[newY][newX] != '#') {
-			player->move(dx, dy);
+		if (newX > 0 && newX < width && newY > 0 && newY < height && field[newY][newX] != 'W') {
 
-			// Оновлюємо поле
-			field[oldY][oldX] = '#'; // Стара позиція гравця стає пустою
-			field[newY][newX] = 'P'; // Нова позиція гравця
+			player->move(dx, dy);
 		}
 		else {
 			newX = oldX;
@@ -72,36 +265,5 @@ public:
 			player->setX(newX);
 			player->setY(newY);
 		}
-	}
-};
-class Player {
-private:
-	int playerX = 5;
-	int playerY = 5;
-	invetory invetory;
-public:
-	void move(int dx, int dy) {
-		playerX += dx;
-		playerY += dy;
-	}
-	
-	void addItem(Item* item) {
-		invetory.addItem(item);
-	}
-
-	bool hasItem(const string& itemName) const {
-		return invetory.containsItem(itemName);
-	}
-	int getX() {
-		return playerX;
-	}
-	int getY() {
-		return playerY;
-	}
-	int setY(int playerY) {
-		this->playerY = playerY;
-	}
-	int setX(int playerX) {
-		this->playerX = playerX;
 	}
 };
